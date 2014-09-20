b6407f12ba9849b164d82a9fceba237772edb0a9
Brendan Ashworth <squirrelslikeacorns@gmail.com>
2014-09-20 11:13:17 -0700

    Adds static viewing

--- app/controllers/UpimgController.php
+++ app/controllers/UpimgController.php
@@ -26,6 +26,20 @@ class UpimgController extends Controller {
 		return View::make('upimg.imgView')->with('img', $img);
 	}
 
+	public function imgRaw($img) {
+		try {
+			$file = File::get('images/' . $img . '.png');
+		} catch ($error) {
+			// Send error
+			return Redirect::to('/')->with('message', "noimg! The file was too large or missing.");
+		}
+
+		$response = Response::make($file, 200);
+		$response->header('Content-Type', mime_content_type($file));
+
+		return $response;
+	}
+
 	public function view() {
 		return View::make('upimg.view');
 	}
--- app/routes.php
+++ app/routes.php
@@ -17,7 +17,8 @@ Route::post('/upload', array('as' => 'image_upload', 'uses' => 'UpimgController@
 
 Route::get('/blog', array('as' => 'upimg_blog', 'uses' => 'UpimgController@blog'));
 
-Route::get('/{img}', array('as' => 'upimg_img', 'uses' => 'UpimgController@imgView'))->where('img', '[a-z0-9]+');
+Route::get('/{img}', array('as' => 'upimg_img', 'uses' => 'UpimgController@imgView'))->where('img', '[a-z0-9]{32}');
+Route::get('/{img}.png', array('as' => 'upimg_img', 'uses' => 'UpimgController@imgRaw'))->where('img', '[a-z0-9]{32}');
 
 Route::get('/view', array('as' => 'upimg_img', 'uses' => 'UpimgController@view'));
 
