9963daa58de9473fe36cf6cd558b3ff2920febd5
Cloyd1815.me <admin@cloyd1815.me>
2014-11-30 16:34:30 -0500

    local

--- app/views/upimg/imgView.blade.php
+++ app/views/upimg/imgView.blade.php
@@ -34,12 +34,10 @@
 
 
 		<div class="container">
-			<div class="col-md-12">
-			<a href="images/{{ $img }}.png" target="_self"><img src="images/{{ $img }}.png" alt="upimg image hosting utility" id="main-image" /></img>
+			<a href="images/{{ $img }}.png" target="_self"><img src="images/{{ $img }}.png" alt="upimg image hosting utility" id="main-image" class="img-responsive" />
 			<hr />
 			<div class="well">
 				<p>Hot link to this resource: <a href="images/{{ $img }}.png">http://upimg.me/images/{{ $img }}.png</a></p>
-			</div>
 			</div>
 		</div>
 	</body>
