7e2d68cc6ccb7dabe279a48185f29c1a192b8f66
apemanzilla <dominic@marcuse.us>
2014-12-06 11:10:58 -0500

    Fix stuff

--- app/views/upimg/index.blade.php
+++ app/views/upimg/index.blade.php
@@ -9,7 +9,7 @@
 	{{ Form::open(array('url' => 'upload', 'files' => true)) }}
 	{{ Form::file('file', $attributes = array('id' => 'file_input', 'type' => 'file')) }}
 	<div class="centered">
-	<button id="file_input_activator" class="btn btn-info btn-large text-center">Choose File</button>
+	<button id="file_input_activator" class="btn btn-info btn-lg text-center">Choose File</button>
 	{{ Form::submit('Upload', $attributes = array('class' =>'btn btn-primary btn-lg text-center')) }}
 	</div>
 	{{ Form::close() }}
