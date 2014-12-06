efdcf3d4ffcd79839d883aa517c89067ae0a2f54
Cloyd1815 <loyd3152@gmail.com>
2014-12-05 20:13:42 -0500

    Removed header on user pages for images without name or description

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -6,10 +6,12 @@
 		<hr class="featurette-divider">
 		@foreach($uploads as $upload)
 		<div class="panel panel-primary">
+		@if($upload->img_name != null or $upload->img_desc != null)
 			<div class="panel-heading">
 				<h1>{{{ $upload->img_name }}}</h1>
 				<h4 class="text-muted">{{{ $upload->img_desc }}}</h4>
 			</div>
+		@endif
 			<a href="{{ $url = asset($upload->file_name) }}">
 				<div class="panel-body">
 					<img id="main-image" src="{{$url = asset('/images/' . $upload->file_name . '.png') }}" />
